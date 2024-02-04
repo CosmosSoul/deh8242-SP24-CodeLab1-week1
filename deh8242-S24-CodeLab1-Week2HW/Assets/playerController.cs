dbdef5757c5a16f4ef6a4e71c7805631fed83265
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-02-05 01:29:23 +0900

    Added player controller script, ground
    now player controller script handles horizontal input
    ground has been added with collider

--- deh8242-S24-CodeLab1-Week2HW/Assets/playerController.cs
+++ deh8242-S24-CodeLab1-Week2HW/Assets/playerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+
+    public float jumpForce = 5.0f;
+
+    public float maxVelocity = 15.0f;
+
+    private Rigidbody2D playerRB;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerRB.AddForce(moveSpeed * horizontalInput * Vector2.right);
     }
 }
