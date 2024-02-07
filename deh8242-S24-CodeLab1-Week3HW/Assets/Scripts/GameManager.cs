242b45aee8ac176561c9399faf939133a1e00e30
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-02-08 06:23:17 +0900

    Added project folder for week3 HW

---
+++ deh8242-S24-CodeLab1-Week3HW/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score = 0;
+
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool gameOver = false;
+    public AudioSource gameAudio;
+    
+
+
+    void Awake()
+    {
+        //QUESTION: Does instance mean we are setting this newly created gameManager to be persistent throughout scenes?
+        //I dont fully understand this logic :( 😢
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameOverText made invisible on the first frame. 
+        gameOverText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
---
+++ deh8242-S24-CodeLab1-Week3HW/Assets/Scripts/deathBoxScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class deathBoxScript : MonoBehaviour
+{
+
+    
+
+    public AudioClip fireSound;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameManager.instance.gameOver = true;
+        GameManager.instance.gameOverText.gameObject.SetActive(true);
+        GameManager.instance.gameAudio.PlayOneShot(fireSound, 1.5f);
+    }
+}
