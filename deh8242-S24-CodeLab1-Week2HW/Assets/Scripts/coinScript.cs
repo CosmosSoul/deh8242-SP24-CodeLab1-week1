920e12ef0b1545abb249db556c8db751885f97a5
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-02-08 06:33:47 +0900

    Added comments describing coin script and tasks to complete later.

--- deh8242-S24-CodeLab1-Week2HW/Assets/Scripts/coinScript.cs
+++ deh8242-S24-CodeLab1-Week2HW/Assets/Scripts/coinScript.cs
@@ -1,18 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Random = UnityEngine.Random;
 
+using System.Collections.Generic;
+using UnityEngine;
 public class coinScript : MonoBehaviour
 {
 
    
     private void OnCollisionEnter2D(Collision2D col)
     {   
-        //score increments by one and new coin position randomized in the y, always ahead of the player
+        //score increments by one and new coin position randomized onn y-axis, always ahead of the player nn x-axis
         GameManager.instance.score++;
         transform.position = new Vector2((transform.position.x + 11), Random.Range(2, 3.5f));
         
         //add additional speed bonus on coin pickup
+        
+        /*add sound on coin pickup*/
     }
 }
