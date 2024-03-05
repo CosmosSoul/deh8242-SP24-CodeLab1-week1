b78c5a60a1293b74d461417491f969bd96daacc5
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-03-04 20:39:46 -0500

    started building click functionality. Made clickAction script with onMouseDown() and attached script to target0 prefab

---
+++ deh8242-S24-CodeLab1-Week6-7midtermHW/Assets/Scripts/clickAction.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class clickAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Debug.Log("you clicked me!");
+    }
+}
