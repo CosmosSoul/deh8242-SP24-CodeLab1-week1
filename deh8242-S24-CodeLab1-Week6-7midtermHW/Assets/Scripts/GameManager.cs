7f844a4b71b371136a223cf3cdfe7d56bca5472e
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-03-05 00:00:20 -0500

    game score and game time now properly display in game scene.

--- deh8242-S24-CodeLab1-Week6-7midtermHW/Assets/Scripts/GameManager.cs
+++ deh8242-S24-CodeLab1-Week6-7midtermHW/Assets/Scripts/GameManager.cs
@@ -146,6 +146,8 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        scoreText.text = "Score: " + Score + "\n Time: " + timer;
         
     }
 }
