b54cef1d39f25ec9f9291c8b79bf6e3dafebd17b
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-03-29 18:39:12 -0400

    started building ScriptableObject script with location placeholder for cardinal directions and created new asset menu item for making new location

--- deh8242-S24-CodeLab1-Week9HW/Assets/Scripts/LocationScriptableObject.cs
+++ deh8242-S24-CodeLab1-Week9HW/Assets/Scripts/LocationScriptableObject.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu (fileName = "Empty Location", menuName = "Empty Location", order = 0)]
 public class LocationScriptableObject : ScriptableObject
 {
   public string locationName;
   public string locationDesc;
 
+  public LocationScriptableObject north;
+  public LocationScriptableObject south;
+  public LocationScriptableObject west;
+  public LocationScriptableObject east;
+  
+  
+
   public void PrintLocation()
   {
-    // string printStr = 
+    string printStr = "\nLocation Name: " + locationName + "\nLocation Description: " + locationDesc;
+    
+    Debug.Log(printStr);
   }
 }
