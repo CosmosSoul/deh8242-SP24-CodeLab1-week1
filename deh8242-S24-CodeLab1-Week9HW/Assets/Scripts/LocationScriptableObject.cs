2bb2f14bbbb5347105f5309042a280906e1b5f44
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-03-29 18:05:03 -0400

    Created Week9 HW project

---
+++ deh8242-S24-CodeLab1-Week9HW/Assets/Scripts/LocationScriptableObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationScriptableObject : ScriptableObject
+{
+  public string locationName;
+  public string locationDesc;
+
+  public void PrintLocation()
+  {
+    // string printStr = 
+  }
+}
