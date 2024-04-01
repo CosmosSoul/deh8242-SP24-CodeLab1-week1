789be10ef4b5aaf25a595137aabe0863d7624c57
CosmosSoul <26847948+CosmosSoul@users.noreply.github.com>
2024-04-01 13:32:45 -0400

    added searchArea(), randomizeItemLocation(), bossLocationChange(), started building penalty for encountering boss: RPS minigame

--- deh8242-S24-CodeLab1-Week9HW/Assets/Scripts/GameManager.cs
+++ deh8242-S24-CodeLab1-Week9HW/Assets/Scripts/GameManager.cs
@@ -10,18 +10,29 @@ using Random = System.Random;
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI locationDescriptionUI;
+    public TextMeshProUGUI bossText;
 
     public LocationScriptableObject currentLocation;
     [FormerlySerializedAs("LocationsArray")] public LocationScriptableObject[] locationsArray;
     public LocationScriptableObject bossLocation;
+    public LocationScriptableObject itemLocation;
      
 
     public Button buttonNorth;
     public Button buttonSouth;
     public Button buttonEast;
     public Button buttonWest;
+    
+    public Button search;
+    public Button rock;
+    public Button paper;
+    public Button scissors;
 
     public bool gameOver = false;
+    public bool hasItem = false;
+    private string bossHand;
+    
+    
    
     
     
@@ -32,9 +43,10 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int randomLocation = UnityEngine.Random.Range(0, locationsArray.Length);
-        bossLocation = locationsArray[randomLocation];
-        Debug.Log(bossLocation);
+        bossEncounterOff();
+        bossLocationChange();
+        randomizeItemLocation();
+        
         
         locationDescriptionUI.text = currentLocation.locationName + "\n" + currentLocation.locationDesc;
         currentLocation.PrintLocation();
@@ -44,17 +56,86 @@ public class GameManager : MonoBehaviour
 
     public void bossLocationChange()
     {
+        int randomBossLocation = UnityEngine.Random.Range(0, locationsArray.Length);
+        bossLocation = locationsArray[randomBossLocation];
+        Debug.Log("the Boss is in" + bossLocation);
+    }
+
+    public void randomizeItemLocation()
+    {
+        int randomItemLocation = UnityEngine.Random.Range(0, locationsArray.Length);
+        itemLocation = locationsArray[randomItemLocation];
+        Debug.Log("The item is in" + itemLocation);
         
     }
 
+    public void bossEncounterOn()
+    {
+        rock.gameObject.SetActive(true);
+        paper.gameObject.SetActive(true);
+        scissors.gameObject.SetActive(true);
+        bossText.text = "What will you throw?";
+    }
+
+    public void bossEncounterOff()
+    {
+        rock.gameObject.SetActive(false);
+        paper.gameObject.SetActive(false);
+        scissors.gameObject.SetActive(false);
+        bossText.gameObject.SetActive(false);
+    }
+
+    public void searchArea()
+    {
+        if (currentLocation == itemLocation)
+        {
+            hasItem = true;
+            Debug.Log("You've found it!");
+        }
+        else
+        {
+            hasItem = false;
+            Debug.Log("No, I don't see it here");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
+        if (currentLocation == bossLocation)
+        {
+            bossEncounterOn();
+            //WHY is this throwing an Error?!?
+            //int randomHand = Random.Range(0, 2);
+
+            int randomHand = UnityEngine.Random.Range(0, 2);
+
+            switch (randomHand)
+            {
+                case 0:
+                    bossHand = "Rock";
+                    break;
+                case 1:
+                    bossHand = "Paper";
+                    break;
+                case 2:
+                    bossHand = "Scissor";
+                    break;
+                default:
+                    Debug.Log("uhh, what is that?");
+                    break;
+            }
+        }
         if (currentLocation == bossLocation && !gameOver)
         {
             gameOver = true;
             Debug.Log("You have to work this weekend!");
             gameOver = false;
+        }
+
+        if (currentLocation == locationsArray[1] && hasItem)
+        {
+            gameOver = true;
+            Debug.Log("You made it out! Enjoy your weekend!");
         }
     }
     
